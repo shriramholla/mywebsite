f6360a03c460d4a92bf4789dcce749d427b8619c
Shriram Holla <bab.shriram@gmail.com>
2020-09-06 22:32:56 +0530

    Updates

---
+++ precache-manifest.08f01564cdd5434097ec153bbeba1c7f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ca2034abf3c8a4dc23d8621d7a8674",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "7a147bef1f9c3731b1e1",
+    "url": "/mywebsite/static/css/main.3e6829a0.chunk.css"
+  },
+  {
+    "revision": "b07ee92db165e60593a7",
+    "url": "/mywebsite/static/js/2.b0d53d20.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/mywebsite/static/js/2.b0d53d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a147bef1f9c3731b1e1",
+    "url": "/mywebsite/static/js/main.2d407682.chunk.js"
+  },
+  {
+    "revision": "8bfdc81bc68db2f39e17",
+    "url": "/mywebsite/static/js/runtime-main.2e72d8b5.js"
+  },
+  {
+    "revision": "49a1d1e3511a05d1dc530bdaf4d686b1",
+    "url": "/mywebsite/static/media/SDD.49a1d1e3.gif"
+  },
+  {
+    "revision": "1230db21f46ac25d404cac5a6a6bc794",
+    "url": "/mywebsite/static/media/ardor.1230db21.gif"
+  },
+  {
+    "revision": "f65553379c1fbdb7674f1791cb98da89",
+    "url": "/mywebsite/static/media/autonet.f6555337.gif"
+  },
+  {
+    "revision": "08e3e2bbd40027d36f70e92f58d559d8",
+    "url": "/mywebsite/static/media/convey.08e3e2bb.gif"
+  },
+  {
+    "revision": "3aa830dbdce5efe31ffe97e4777d36a2",
+    "url": "/mywebsite/static/media/coronah.3aa830db.gif"
+  },
+  {
+    "revision": "d4e56f264976a43c88bcf4b5a8b8b9c1",
+    "url": "/mywebsite/static/media/daily.d4e56f26.gif"
+  },
+  {
+    "revision": "d24b0c31077e97b7194a698e0d7ad2bd",
+    "url": "/mywebsite/static/media/face.d24b0c31.jpg"
+  },
+  {
+    "revision": "dd3e213a39b82621cec55d4ae9ff77e7",
+    "url": "/mywebsite/static/media/inventory.dd3e213a.gif"
+  },
+  {
+    "revision": "8bd895017f75a08ecd26c6c3f77e7fa9",
+    "url": "/mywebsite/static/media/myimg.8bd89501.jpg"
+  },
+  {
+    "revision": "50effa3f7083e94a2d55857ad0da0328",
+    "url": "/mywebsite/static/media/myresume.50effa3f.pdf"
+  },
+  {
+    "revision": "80a74cd80ce7191b835c8e609cd1e12c",
+    "url": "/mywebsite/static/media/sudoku.80a74cd8.gif"
+  },
+  {
+    "revision": "1fcdf130459085995042ebbe345a7775",
+    "url": "/mywebsite/static/media/ted.1fcdf130.gif"
+  },
+  {
+    "revision": "0b712f20ff39d29bad099d864ae49f82",
+    "url": "/mywebsite/static/media/thermal.0b712f20.gif"
+  },
+  {
+    "revision": "a9eb84fcd1b59f8f065e635564fc7caa",
+    "url": "/mywebsite/static/media/titanic.a9eb84fc.jpg"
+  }
+]);
